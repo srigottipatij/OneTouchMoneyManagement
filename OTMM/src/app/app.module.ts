fda07667859b041069be41ecd93064a926fd7644
Suguna sri <srigottipatij@gmail.com>
2018-09-07 14:51:24 +0530

    firebase basic module and database module sucessfully integrated

--- OTMM/src/app/app.module.ts
+++ OTMM/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {AngularFireModule} from 'Angularfire2'; //Sri: imported basic module to work angular with firebase
-
-
+import { AngularFireModule } from 'angularfire2'; //Sri: imported basic module to work angular with firebase
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
@@ -14,6 +13,8 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),  //Sri: initializing our application with the firebase configuration (which we provided in environment.ts which we actually copied from our project setup in firebase)    
+    AngularFireDatabaseModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
