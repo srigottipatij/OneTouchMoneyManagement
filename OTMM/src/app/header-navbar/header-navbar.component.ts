573772332e3c64d7563ce590d5b516ed38f0704f
Suguna sri <srigottipatij@gmail.com>
2018-12-05 22:28:43 +0530

    htmls:

--- OTMM/src/app/header-navbar/header-navbar.component.ts
+++ OTMM/src/app/header-navbar/header-navbar.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+import { UserService } from '../services/user.service';
+import { OtmmAuth } from '../services/otmm-auth.service';
 
 @Component({
   selector: 'header-navbar',
   templateUrl: './header-navbar.component.html',
   styleUrls: ['./header-navbar.component.css']
 })
-export class HeaderNavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class HeaderNavbarComponent{  
+  constructor(private otmmAuth: OtmmAuth){ //I used otmmAuth in header-navbar.component.ts, thats why I have injected it   
   }
 
 }
