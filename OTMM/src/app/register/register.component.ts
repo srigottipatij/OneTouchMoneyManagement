2cdd3c7ab260b3a2dfb87a96965f42b813e80bce
Suguna sri <srigottipatij@gmail.com>
2018-12-06 19:54:20 +0530

    register.component.ts - reactive form declaration in controller using FormGroup, form element declaration using FormControl, and binded with inbuilt validators (these inbuilt validators are not html 5 validations, but works similarly), used userdefined asynchronous validator, used updateOn blur over validations so that the validators and asynchornous validators will be updated on blur i.e. called upon blur, creating sub form group, sub group userdefined validator, subscribing observable, navigation using router and also passing data through query parameters, getters; register.component.html - add reactive form declaration in html and make sure the name of form matches with that of the controller, making an input text field as formcontrol using formControlName when using reactive forms, reactive form validations and *ngIf, inputformmatter, displaying dynamically the requiredLength value instead of hardcoding, asynchronous validation error checking and displaying error message, subform group is declared using formGroupName in reactive forms, checking for group validation, and displaying error message when validation fails, disabling the register button, when form is not valid

--- OTMM/src/app/register/register.component.ts
+++ OTMM/src/app/register/register.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AppUser } from '../models/app-user';
-import { OtmmAuth } from '../otmm-auth.service';
+import { OtmmAuth } from '../services/otmm-auth.service';
+import { UserService } from '../services/user.service';
+import {FormControl,FormGroup, Validators} from '@angular/forms';
+import { PasswordValidators } from '../validators/password.validators';
+import { Router } from '@angular/router';
+import { EmailValidators } from '../validators/email.validators';
+
 
 @Component({
   selector: 'app-register',
@@ -8,17 +14,48 @@ import { OtmmAuth } from '../otmm-auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent{
+  signupform=new FormGroup({ //reactive form declaration in controller using FormGroup
+    'name':new FormControl('',[Validators.required,Validators.minLength(8),Validators.maxLength(50)]), //form element name declaration using FormControl, binded with inbuilt validators (these are not html 5 validations, but works similarly)
+    'email': new FormControl('',
+    {validators: [Validators.required,Validators.minLength(8),Validators.maxLength(50),Validators.email], //form element email
+      asyncValidators: [EmailValidators.uniqueUser],updateOn: 'blur' //used userdefined asynchronous validator, used updateOn blur over validations so that the validators and asynchornous validators will be updated on blur i.e. called upon blur
+    }),
+    passwordgroup: new FormGroup({ //creating sub form group
+      'password': new FormControl('',[Validators.required,Validators.minLength(8),Validators.maxLength(50)]),
+      'cpassword': new FormControl('',[Validators.required])},
+      PasswordValidators.confirmPassword)    // sub group userdefined validator,  
+  });
+  constructor(private auth: OtmmAuth,private userService: UserService,private router: Router) { }
 
-  appUser: AppUser;
-  constructor(private auth: OtmmAuth) { 
-
-  }
-
-  register(user){           
-   if(!this.auth.registerUser(user))
-     console.log("Error: user already exist!!");  
-     else
-     console.log("Successfully Registered user with email id:"+user.email);              
+  register(user){  
+    let userDetails: AppUser;
+    
+    userDetails=user;
+        
+    userDetails.Password=user.passwordgroup.password;
+    
+    
+    this.auth.registerUser(user).subscribe(newuser=> //subscribe observable
+      {        
+        if(newuser){   
+          let appUser=new AppUser(newuser);          
+          this.router.navigate(['/login'],{queryParams:{ email: appUser.email, name: appUser.name}});     //navigation using router and also passing data through query parameters     
+          return true;                
+        }                            
+    },
+  error=>{          
+    console.log("EE:",error);
+  });          
+     
   }  
 
+  get email(){ //getter
+    return this.signupform.get('email');
+  }
+  get passwordgroup(){ //getter
+    return this.signupform.get('passwordgroup');
+  }
+  get password(){ //getter
+    return this.signupform.get('passwordgroup').get('password');
+  }
 }
