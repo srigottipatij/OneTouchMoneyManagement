2df48b45c43f6441fa95292b52a5b23be29b6f0d
Suguna sri <srigottipatij@gmail.com>
2018-12-06 19:27:44 +0530

    login.component.ts- ActivatedRoute, Router,  getting queryparam by using ActivatedRoute snapshot route snapshot with queryParamMap, navigating using router, used getters; login.component.html - template-driven form (ngForm), form validations, asynchronous form validation when submitted form, validation error messages, used class bindings,  default value to input field using ngModel and interpollation, and also used input formatter, button disabling unless all form values are correct, event binding, style binding, template variables, template variable email is assigned with ngModel, interpolation, *ngIf

--- OTMM/src/app/login/login.component.ts
+++ OTMM/src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '../../../node_modules/@angular/router';
-import { OtmmAuth } from '../otmm-auth.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { OtmmAuth } from '../services/otmm-auth.service';
+import { HeaderNavbarComponent } from '../header-navbar/header-navbar.component';
+import { UserService } from '../services/user.service';
+import { AppUser } from '../models/app-user';
 
 @Component({
   selector: 'app-login',
@@ -8,20 +11,42 @@ import { OtmmAuth } from '../otmm-auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  userState;
+  loginstatus: boolean=true;
+  constructor(private router: Router,private route: ActivatedRoute,private auth: OtmmAuth,private userService: UserService) //ActivatedRoute, Router
+  { }
+  
+  headerNavbar: HeaderNavbarComponent;
+ 
+  login(userCredentials){   
+    
+    this.auth.loginUser(userCredentials).subscribe(resultUserSession=> //subscribe observable
+      {     
+            
+        this.userState=resultUserSession;
+        this.loginstatus=true;   
+         
+        let returnUrl=this.route.snapshot.queryParamMap.get('returnUrl'); //getting queryparam using route snapshot with queryParamMap
+        
+        this.router.navigate([returnUrl || '/']);    //navigating using router
+        return true; 
+      },
+      error=>{          
+        console.log("EE:",error);
+      });
 
-  constructor(private router: Router,private auth: OtmmAuth) { }
-
-  login(user){
-    if(this.auth.loginUser(user))
-    {
-      console.log("Logined");
-      this.router.navigateByUrl("/user/manage-money");
-     }
-     else
-     {
-     console.log("Invalid Username/Password");
-     
-     }
+    
+    this.loginstatus=false; // we set this to toggle validation error message
+    
+    return false;
+                 
   }
 
+  get userEmail(){ //getter
+    return this.route.snapshot.queryParamMap.get('email'); //getting queryparam using route snapshot with queryParamMap
+  }  
+  get userName(){ //getter
+    return this.route.snapshot.queryParamMap.get('name'); //getting queryparam using route snapshot with queryParamMap
+  }
+  
 }
